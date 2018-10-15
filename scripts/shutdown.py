d96f9791de992b4dc39fc5e2ded85f08ab504faa
Martin Gurtner <martin.gurtner@gmail.com>
2018-10-15 15:51:29 +0200

    power-off script modified so that it lits up the power LED

--- scripts/shutdown.py
+++ scripts/shutdown.py
@@ -10,6 +10,11 @@ GPIO.setmode(GPIO.BOARD)
 # use the same pin that is used for the reset button (one button to rule them all!)
 GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Switch on the power LED
+led = 19
+GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, 1)
+
 oldButtonState1 = True
 
 while True:
@@ -20,6 +25,8 @@ while True:
     if buttonState1 != oldButtonState1 and buttonState1 == False:
         # shutdown
         subprocess.Popen(['poweroff'])
+        # Switch off the power LED
+        GPIO.output(led, 0)
         oldButtonState1 = buttonState1
 
     time.sleep(.5)
